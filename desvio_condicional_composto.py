d7f9e97bdad5fdb94b336dad121cc163c8b0f253
Pedro Mamed <mamed14.pedro@gmail.com.br>
2023-08-14 11:48:25 -0300

    Aula sobre desvio condicional

---
+++ desvio_condicional_composto.py
@@ -0,0 +1,12 @@
+'''
+Crie uum programa que pergunte a idade da pessoa.
+Em seguida o program deve informar se a pessoa é menor e idade ou se é maior de idade.
+'''
+
+idade = int(input("Qual é a sua idade? "))
+
+if ( idade < 18):
+    print("Você é menor de idade!")
+else:
+    print("Você é maior de idade!")
+print("FIM DO PROGRAMA")
